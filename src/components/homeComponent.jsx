a1dec82b095a5b43bf1a3b22f5d128c3717b682a
EdyRivas <alexicho03@gmail.com>
2023-01-24 23:40:12 -0700

    cartas base personajes

--- src/components/homeComponent.jsx
+++ src/components/homeComponent.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './../styles/components/homeComponent/homeComponent.sass'
 
 const Cards = () => {
 
@@ -7,11 +7,67 @@ const Cards = () => {
     <div className="cont">
       <div className="half">
         <div className="cart">
-
+          <div className="imgcont">
+            <img src="" alt="" />
+          </div>
+          <div className="rest">
+            <div className="rowCont">
+              <p className="smal">Status</p>
+              FavIcon
+            </div>
+            <div className="rowCont">
+              <h3>Name</h3>
+            </div>
+            <div className="rowCont">
+              <p className="bold">Cumpleaños:</p>
+              <p>data</p>
+            </div>
+            <div className="rowCont">
+              <p className="bold">Genero:</p>
+              <p>data</p>
+            </div>
+            <div className="rowCont">
+              <p className="bold">Color de ojos:</p>
+              <p>data</p>
+            </div>
+            <div className="rowCont">
+              <p className="bold">Color de pelo:</p>
+              <p>data</p>
+            </div>
+          </div>
         </div>
       </div>
       <div className="half">
-
+        <div className="cart">
+          <div className="imgcont">
+            <img src="" alt="" />
+          </div>
+          <div className="rest">
+            <div className="rowCont">
+              <p className="smal">Status</p>
+              FavIcon
+            </div>
+            <div className="rowCont">
+              <h3>Name</h3>
+            </div>
+            <div className="rowCont">
+              <p className="bold">Cumpleaños:</p>
+              <p>data</p>
+            </div>
+            <div className="rowCont">
+              <p className="bold">Genero:</p>
+              <p>data</p>
+            </div>
+            <div className="rowCont">
+              <p className="bold">Color de ojos:</p>
+              <p>data</p>
+            </div>
+            <div className="rowCont">
+              <p className="bold">Color de pelo:</p>
+              <p>data</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
