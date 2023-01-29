2a81393abfb49118c94a6a87cd4d65eb47c985eb
EdyRivas <alexicho03@gmail.com>
2023-01-29 01:10:47 -0700

    top bar movile view

--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -6,22 +6,30 @@ import { faBookmark, faUserPlus } from '@fortawesome/free-solid-svg-icons'
 const TopBar = () => {
 
   document.addEventListener('click', (event) => {
+    event.preventDefault();
     let toggle = document.getElementById('toggle');
-    if (event.target.id === 'openFav') {
-      if (!toggle.classList.contains('show')) {
-        toggle.style.animation = '.4s ease open forwards';
-        toggle.classList.add('show');
-        return;
+    if (toggle) {
+
+      if (event.target.id === 'openFav') {
+        if (!toggle.classList.contains('show')) {
+          toggle.style.animation = '.4s ease open forwards';
+          toggle.style.boxShadow = '0 0 3px 2px #1010105b'
+          toggle.classList.add('show');
+          return;
+        }
+        else {
+          toggle.classList.remove('show');
+          toggle.style.animation = '.4s ease close forwards';
+          toggle.style.boxShadow = ''
+          return
+        }
       }
-      else {
-        return
-      }
-    }
-    if (toggle)
-      if (event.target.id !== 'toggle' && toggle.classList.contains('show')) {
+      else if (event.target.id !== 'toggle' && toggle.classList.contains('show')) {
         toggle.classList.remove('show');
         toggle.style.animation = '.4s ease close forwards';
+        toggle.style.boxShadow = ''
       }
+    }
   });
   return (
     <div className="btnsComt">
