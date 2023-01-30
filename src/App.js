b8d547bc3fd10c94e9af00b150a339f07946cce0
EdyRivas <alexicho03@gmail.com>
2023-01-29 20:33:50 -0700

    landing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     return (
         <BrowserRouter>
             <Routes >
-                <Route path="/am-test" element={<Home />} />
+                <Route path="/" element={<Home />} />
             </Routes>
         </BrowserRouter >
     );
