c8de61143b8681947d82975f3db8d92ebd1c924f
EdyRivas <alexicho03@gmail.com>
2023-01-29 20:37:05 -0700

    landing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./pages/home";
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes >
                 <Route path="/" element={<Home />} />
             </Routes>
-        </BrowserRouter >
+        </HashRouter >
     );
 }
 
